973c0915ce78ff7fa1bc730f6d79f331f8a77e48
kunal-kumar11 <135869954+kunal-kumar11@users.noreply.github.com>
2025-03-28 13:35:59 +0530

    Create custom_middlewarepart2.js

---
+++ custom_middlewarepart2.js
@@ -0,0 +1,22 @@
+const express= require('express')
+
+const app=express()
+
+
+app.use((req,res,next)=>{
+    
+    req.user="Guest"
+
+    next()
+})
+
+
+app.get('/welcome',(req,res)=>{
+
+    res.send(`<h1>Welcome,${req.user} </h1>`)
+
+})
+
+app.listen(3400,()=>{
+    console.log("Server it active at port 3400")
+})

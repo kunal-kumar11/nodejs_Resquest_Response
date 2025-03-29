9c6680377d592e6ee2567ef7d16126466b44ad49
kunal-kumar11 <135869954+kunal-kumar11@users.noreply.github.com>
2025-03-29 13:52:17 +0530

    Update app.js

--- ExpressJs/NowWorkingWithViews/app.js
+++ ExpressJs/NowWorkingWithViews/app.js
@@ -6,8 +6,14 @@ const app=express()
 
 app.get('/',(req,res)=>{
 
+   console.log("WELCOME TO HTML CONTENT")
+})
+
+app.get('/api/products',(req,res)=>{
+
     res.sendFile(path.join(__dirname,"views","index.html"))
 })
+
 
 const port=4800
 app.listen(port,()=>{
